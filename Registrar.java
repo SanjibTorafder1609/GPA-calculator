2486b0b255c5035cf741c4f1414c8eb7ad675bca
SanjibTorafder <sagorseptorafder@gmail.com>
2022-09-22 01:36:54 -0300

    Removed ambiguity
    also added comments to explain getGPA method

--- Registrar.java
+++ Registrar.java
@@ -1,3 +1,4 @@
+package assn1.syt;
 import java.util.List;
 
 public class Registrar{
@@ -5,13 +6,12 @@ public class Registrar{
     public static double getGPA(List<Grade> grades){
         double totalch = 0;
         double totalgp = 0;
-
+        
         for (int i = 0; i<grades.size(); i++){
-            double tempch = grades.get(i).getCreditNum();
-            // changed
-            double ch = (tempch - '0');
-            totalch += ch;
-            totalgp += (grades.get(i).getGradePoint())*ch;
+            double tempCreditNum = grades.get(i).getCreditNum();   //creditNum is stored as a ch therefore changing to a double
+            double finalCreditNum = (tempCreditNum - '0');     //after conversion the double is 52 therefore subtracting 48 (char '0')
+            totalch += finalCreditNum;
+            totalgp += (grades.get(i).getGradePoint())*finalCreditNum;
         }
         return totalgp/totalch;
     }
